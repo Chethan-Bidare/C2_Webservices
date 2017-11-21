649fd72cfc2d5fa54b0f90600818502d9fd7852d
chetan <chetan.gmail.com>
2017-11-21 11:22:02 +0530

    added methods

--- src/test/java/c2info_Webservices/methods/Webservices.java
+++ src/test/java/c2info_Webservices/methods/Webservices.java
@@ -1,5 +1,39 @@
 package c2info_Webservices.methods;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+
 public class Webservices {
+	
+	public static Response Post(String uRI,String stringJSON){
+		RequestSpecification requestspecification = RestAssured.given().body(stringJSON);
+		requestspecification.contentType(ContentType.JSON);
+		Response response = requestspecification.post(uRI);
+		return response ;
+	}
+	
+	public static Response Get(String uRI){
+		RequestSpecification requestspecification = RestAssured.given();
+		requestspecification.contentType(ContentType.JSON);
+		Response response = requestspecification.get(uRI);
+		return response ;
+		
+	}
+	
+	public static Response Put(String uRI,String stringJSON){
+		RequestSpecification requestspecification = RestAssured.given().body(stringJSON);
+		requestspecification.contentType(ContentType.JSON);
+		Response response = requestspecification.put(uRI);
+		return response ;
+	}
+	
+	public static Response Delete(String uRI){
+		RequestSpecification requestspecification = RestAssured.given();
+		requestspecification.contentType(ContentType.JSON);
+		Response response = requestspecification.put(uRI);
+		return response ;
+	}
 
 }
